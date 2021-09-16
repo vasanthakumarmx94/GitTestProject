df7183905e509c234a6ebc480e4d8c0bf6c7b32e
vasanthakumarmx94 <vasanthakumarmx94@gmail.com>
2021-09-16 12:45:11 +0530

    first commit Commit

---
+++ GitTestProject/src/com/neosoft/GitTestDemo.java
@@ -0,0 +1,18 @@
+package com.neosoft;
+
+public class GitTestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.print("GitTest Testing1");
+      String str="SE";
+      if(str=="SE")
+      {
+    	  System.out.print("Software engneer");
+      }
+      else {
+    	  System.out.print("Not A software Engineer");
+      }
+	}
+
+}
